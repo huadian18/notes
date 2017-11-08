d9b529c2538023234cdabb5868d1a8a5bf8ff218
wushijie <451164037@qq.com>
2017-11-08 18:12:37 +0800

    去除无用代码

--- php/test.php
+++ php/test.php
@@ -1,16 +1,3 @@
 <?php
-function getHttpType(){
-    return 'https';
-}
 
-//处理域名的http
-//function handleSsl($url = '')
-//{
-//    if(getHttpType()=='http'){
-//        return $url;
-//    }
-//    return str_replace('http', getHttpType(), $url);
-//}
-//
-//var_dump(handleSsl());
 
